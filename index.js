76eee67892999e1952895c618204566f7d573232
mightyfavour <105499935+mightyfavour@users.noreply.github.com>
2025-09-17 15:51:53 +0000

    js new class

---
+++ index.js
@@ -0,0 +1,76 @@
+// const grade = "G";
+
+// switch (grade) {
+//   case "A":
+//     console.log("Excellent! 🏆");
+//     break;
+
+//   case "B":
+//     console.log("Well done! 👍");
+//     break;
+
+//   case "C":
+//     console.log("Good, but you can do better 🙂");
+//     break;
+
+//   case "D":
+//     console.log("Keep trying, don’t give up 💪");
+//     break;
+
+//   case "F":
+//     console.log("Failed 😢. Study harder next time.");
+//     break;
+
+//   default:
+//     console.log("Invalid grade entered ❌");
+// }
+
+
+
+// let day = 7;
+// switch (day) {
+//     case 1:
+//         console.log("Monday")
+//         break;
+//     case 2:
+//         console.log("Tueday")
+//         break;
+//     case 3:
+//         console.log("Wednesday")
+//         break;
+//     case 4:
+//         console.log("Thursday")
+//         break;
+//     case 5:
+//         console.log("Friday")
+//         break;
+//     case 6:
+//         console.log("Saturday")
+//         break;
+//     case 7:
+//         console.log("Sunday")
+//         break;
+
+//     default:
+//         console.log("Invalid day")
+// }
+
+// // statement
+// let age = 10;
+
+// // expression
+// 5 + 3 = 8;
+
+// let sum = 5 + 8;
+
+// if you === "Girl"{
+//     go inside;
+// }else {
+//     come outside;
+// }
+
+// condition ? expression1 : expression2;
+let age = 12;
+let canVote = age >= 18? "yes, can vote..." : "can not vote";
+console.log(canVote);
+
